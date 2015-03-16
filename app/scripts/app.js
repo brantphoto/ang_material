15b78d9ac512ec21c80656783f71210f172b7cac
brantphoto <brant.barger@gmail.com>
2015-03-15 23:59:57 -0700

    ready to launch

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,7 @@ angular.module('myApp', ['ngMaterial', 'ngAnimate', 'ngMessages', 'ngAria','zumb
 
 angular.module('myApp')
   .controller('appCtrl', ['$scope', '$interval', '$mdDialog',
-      '$timeout', function($scope, $interval, $mdDialog, $timeout){
+      '$timeout', '$mdMedia', function($scope, $interval, $mdDialog, $timeout, $mdMedia){
 
         vm = this;
         vm.photos = [
@@ -83,16 +83,19 @@ angular.module('myApp')
         // after this line VIDEO POPUP LOGIC
 
         vm.showAdvanced = function(ev) {
-          $mdDialog.show({
-            controller: DialogController,
-            template: '<md-dialog><iframe width="560" height="315" src="https://www.youtube.com/embed/1AKslgGPwnY?autoplay=1" frameborder="0" allowfullscreen></iframe></md-dialog>',
-            targetEvent: ev,
-          })
-          .then(function(answer) {
-            $scope.alert = 'You said the information was "' + answer + '".';
-          }, function() {
-            $scope.alert = 'You cancelled the dialog.';
+          if ($mdMedia('sm')) {
+            $mdDialog.show({
+              controller: DialogController,
+              template: '<md-dialog><iframe width="100%" src="https://www.youtube.com/embed/1AKslgGPwnY?autoplay=1" frameborder="0" allowfullscreen></iframe></md-dialog>',
+              targetEvent: ev
+            });
+          } else {
+            $mdDialog.show({
+              controller: DialogController,
+              template: '<md-dialog><iframe width="560" height="315" src="https://www.youtube.com/embed/1AKslgGPwnY?autoplay=1" frameborder="0" allowfullscreen></iframe></md-dialog>',
+              targetEvent: ev
           });
+          }
         };
 
         vm.triggerHide = false;
@@ -118,6 +121,9 @@ function DialogController($scope, $mdDialog) {
     $mdDialog.hide(answer);
   };
 }
+
+ DialogController.$inject = ['$scope', '$mdDialog'];
+
 
 
 angular.module('myApp')
@@ -210,69 +216,4 @@ angular.module('myApp')
           }
         }
       });
-
-
-    // angular.module('myApp')
-    //   .animation('.slide-image-animation', function () {
-    //     return {
-    //       beforeAddClass: function (element, className, done) {
-    //         if (className == 'ng-hide') {
-    //           var scope = element.scope();
-    //           element.css('opacity',0);
-    //           TweenMax.to(element, 1, {css:{opacity:1}, ease: Ease.easeInOut, onComplete: done});
-    //         }
-    //         else {
-    //           done();
-    //         }
-    //       },
-    //       removeClass: function (element, className, done) {
-    //         if (className == 'ng-hide') {
-    //           // var scope = element.scope(),
-    //           // startPoint = element.parent().width(),
-    //           // tl = new TimelineLite();
-
-    //           // console.log(tl);
-    //           // //if(scope.direction === 'right') startPoint = -startPoint;
-    //           // tl.fromTo(element.find('slide-image'), 3, {alpha:1}, {alpha:0, ease: Ease.easeInOut, onComplete: done});
-
-    //           //tl.fromTo(element, 0.5, { left: startPoint}, {left:0, ease: Ease.easeInOut, onComplete: done})
-    //             //.fromTo(element.find('.title'), 0.5, { left: 800,  alpha: 0}, {left:0, alpha:1, ease:Ease.easeInOut} )
-    //             //.fromTo(element.find('.subtitle'), 0.5, { left: -200, alpha: 0}, {left:0, alpha:1, ease:Ease.easeInOut} )
-    //             //.fromTo(element.find('.avatar'), 0.5, { left: 800, alpha: 0}, {left:300, alpha:1, ease:Ease.easeInOut} );
-    //           }
-    //           else {
-    //               done();
-    //           }
-    //         }
-    //     };
-    //   });
-
-  //angular.module('myApp')
-    //.directive('pwCheck', function() {
-
-      //return {
-        //require: 'ngModel',
-        //scope: {
-            //otherModelValue: '=pwCheck'
-        //},
-        //link: function(scope, element, attributes, ctrl) {
-
-          //ctrl.$validators.pwCheck = function(modelValue) {
-            //if (ctrl.$isEmpty(modelValue)) {
-              //// consider empty models to be valid
-              //return true;
-            //}
-            //return modelValue === scope.otherModelValue;
-          //};
-        //}
-      //};
-    //});
-
-//angular.module('myApp')
-  //.directive("scroll", function ($window) {
-    //return function(scope, element, attrs) {
-
-    //element.$observe(jj
-    //};
-  //});
 
